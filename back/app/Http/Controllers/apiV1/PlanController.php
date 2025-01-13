c4a4c7eb8d0e412cbae3152a555775bfc015c66a
IgnacioToledoDev <ig.toledot@duocuc.cl>
2025-01-12 22:24:44 -0300

    -feat - add admin panel

--- back/app/Http/Controllers/apiV1/PlanController.php
+++ back/app/Http/Controllers/apiV1/PlanController.php
@@ -3,9 +3,23 @@
 namespace App\Http\Controllers\apiV1;
 
 use App\Http\Controllers\Controller;
+use App\Models\Plan;
+use Illuminate\Http\JsonResponse;
 
 class PlanController extends Controller
 {
-    public function getPlans() {
+    public function getPlans(): JsonResponse
+    {
+        $plans = Plan::all();
+
+        if (empty($plans)) {
+            return $this->sendError('plans not found');
+        }
+
+        $success = [
+            'plans' => $plans
+        ];
+
+        return $this->sendResponse($success, 'Plans retrieved successfully.');
     }
 }
