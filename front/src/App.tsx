f5548ef9acf7bb02adcd2ab0b01e9fc34b8c6229
IgnacioToledoDev <ig.toledot@duocuc.cl>
2025-01-31 22:41:05 -0300

    feat - refactor routes

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,26 +1,5 @@
-import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from "react-router-dom"
-import Home from "@/pages/Home"
-import Layout from "./pages/Layout"
-import Login from "./pages/Login"
-import Register from "./pages/Register"
-import { Payment } from "./pages/Payment"
-import DashboardPage from "./pages/Dashboard"
-
-interface RouteHandle {
-  hideHeader?: boolean;
-}
-
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path="/" element={<Layout />} >
-      <Route index path="/" element={<Home />} />
-      <Route path="/login" element={<Login />} handle={{ hideHeader: true} as RouteHandle} />
-      <Route path="/register" element={<Register />} handle={{ hideHeader: true} as RouteHandle} />
-      <Route path="/payment" element={<Payment />} handle={{ hideHeader: true} as RouteHandle} />
-      <Route path="/dashboard" element={<DashboardPage />} handle={{ hideHeader: true} as RouteHandle} />
-    </Route>
-  )
-)
+import { RouterProvider } from "react-router-dom"
+import router from "./routes/Router"
 
 function App() {
   return (
